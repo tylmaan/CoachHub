2672382c8b2c310ecb07a2649c3b17baf9e3c575
tylmaan <kubaty2004@gmail.com>
2026-08-19 23:00:33 +0200

    feat: add EF Core with SQLite and ApplicationDbContext

--- backend/CoachHub.Api/Program.cs
+++ backend/CoachHub.Api/Program.cs
@@ -1,14 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<CoachHub.Api.Data.ApplicationDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
