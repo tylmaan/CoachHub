8a4cdb9999ac92c2c6b1fb5057749307723b6606
tylmaan <kubaty2004@gmail.com>
2026-09-19 19:59:53 +0200

    feat: add team creation form for Admin

--- frontend/src/api/teamsApi.ts
+++ frontend/src/api/teamsApi.ts
@@ -5,3 +5,8 @@ export async function getTeams(): Promise<Team[]> {
     const response = await axiosInstance.get<Team[]>("/teams");
     return response.data;
 }
+
+export async function createTeam(team: Omit<Team, "id">): Promise<Team> {
+    const response = await axiosInstance.post<Team>("/teams", team);
+    return response.data;
+}
